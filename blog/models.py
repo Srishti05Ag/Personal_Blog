5fbbab6860b08ffce08fa780b43c71196953dc46
unknown <srishtiagrwl0502@gmail.com>
2021-04-30 23:08:24 +0530

    front end more

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=255, null=True)
+    body = models.TextField(max_length=2000, null=True)
+    image = models.ImageField(upload_to='images/', null=True)
 
     def __str__(self):
         return self.title
