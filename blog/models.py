de35db13c320d06eb0cdc9d48c5598c8278bc1b5
unknown <srishtiagrwl0502@gmail.com>
2021-05-01 21:08:49 +0530

    summary added

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,9 @@ class Blog(models.Model):
     title = models.CharField(max_length=255, null=True)
     poem = models.ImageField(upload_to='images/', null=True)
     image = models.ImageField(upload_to='images/', null=True)
+    summary = models.TextField(max_length=500, null=True)
 
     def __str__(self):
         return self.title
+
 
--- hindiblog/models.py
+++ hindiblog/models.py
@@ -5,6 +5,7 @@ class HindiBlog(models.Model):
     title = models.CharField(max_length=255, null=True)
     poem = models.ImageField(upload_to='images/', null=True)
     image = models.ImageField(upload_to='images/', null=True)
+    summary = models.TextField(max_length=2000, null=True)
 
     def __str__(self):
         return self.title
