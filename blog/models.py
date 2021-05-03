d22ed249d6459a679ff25dbc7eb4b7252c698019
unknown <srishtiagrwl0502@gmail.com>
2021-05-03 12:50:54 +0530

    linebreaks done

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255, null=True)
-    poem = models.ImageField(upload_to='images/', null=True)
     image = models.ImageField(upload_to='images/', null=True)
-    summary = models.TextField(max_length=500, null=True)
+    poem_text = models.TextField(max_length=2000, null=True)
 
     def __str__(self):
         return self.title
-
+    
 
--- hindiblog/models.py
+++ hindiblog/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 class HindiBlog(models.Model):
     title = models.CharField(max_length=255, null=True)
-    poem = models.ImageField(upload_to='images/', null=True)
     image = models.ImageField(upload_to='images/', null=True)
-    summary = models.TextField(max_length=2000, null=True)
+    poem_text = models.TextField(max_length=2000, null=True)
 
     def __str__(self):
         return self.title
