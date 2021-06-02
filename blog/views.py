2d234daba7e99a7ed38a66139d49086162786b70
Srishti Agrawal <srishtiagrwl0502@gmail.com>
2021-06-02 22:35:28 +0530

    Commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 from .models import Blog
 
 def home(request):
+
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email_address = request.POST.get('email_address')
+        message = request.POST.get('message')
+
+        data={
+            'first_name':first_name,
+            'last_name':last_name,
+            'email_address':email_address,
+            'message':message
+        }
+        message = '''
+        New message: {}
+
+        From: {}
+
+        First Name: {}
+
+        Last Name: {}
+        '''.format(data['message'], data['email'], data['first_name'], data['last_name'])
+        
+        send_mail(data['first_name'], data['last_name'], message, '', ['sweetsrishtiagrawal09@gmail.com'])
+        return HttpResponse('Thanks !!! Your valuable response has been received. We will be in touch at the earliest')
+
     return render(request, 'blog/home.html')
 
 def blogdetail(request, blog_id):
@@ -14,3 +42,29 @@ def englishpoem(request):
     return render(request, 'blog/englishpoem.html', {'blogs':blogs})
 
 
+# def contact(request):
+#     if request.method == 'POST':
+#         first_name = request.POST.get('first_name')
+#         last_name = request.POST.get('last_name')
+#         email_address = request.POST.get('email_address')
+#         message = request.POST.get('message')
+
+#         data={
+#             'first_name':first_name,
+#             'last_name':last_name,
+#             'email_address':email_address,
+#             'message':message
+#         }
+#         message = '''
+#         New message: {}
+
+#         From: {}
+
+#         First Name: {}
+
+#         Last Name: {}
+#         '''.format(data['message'], data['email'], data['first_name'], data['last_name'])
+        
+#         send_mail(data['first_name'], data['last_name'], message, '', ['sweetsrishtiagrawal09@gmail.com'])
+#         return HttpResponse('Thanks !!! Your valuable response has been received. We will be in touch at the earliest')      
+#     return render(request, "blog/home.html", {})
--- mypersonalblog/settings.py
+++ mypersonalblog/settings.py
@@ -135,3 +135,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_HOST_USER= 'sweetsrishtiagrawal09@gmail.com'
+EMAIL_HOST_PASSWORD= 'igwpcqfdmdplkwnr'
+EMAIL_USE_TLS= True
+EMAIL_PORT= 587
+EMAIL_USE_SSL=False
